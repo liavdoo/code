dca3977f85d4bbc85836264381c558d757065f4c
liavdoo <144444098+liavdoo@users.noreply.github.com>
2023-11-12 00:47:21 +0300

    Add files via upload

---
+++ Master.h
@@ -0,0 +1,12 @@
+#ifndef MASTER_H
+#define MASTER_H
+
+class Master{ //написать вывод фраз?
+private: 
+    int Randomizer(int num);
+public:
+    Master();
+    int Baraban(); //выдача очков
+};
+
+#endif
